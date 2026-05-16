86293f0b8fbd7392b07b596f72a51ca62a9de3c5
ICEGODFTBL <icey4934@gmail.com>
2026-05-16 13:56:54 +0200

    Create list.lua

---
+++ icons/list.lua
@@ -0,0 +1,18 @@
+-- arrows
+--[[
+  12338898430 -- arrow down
+  12338895342 -- arow right 
+  12338896710 -- arrow left
+  12338897588 -- arrow up
+
+-- phone
+5261794470 -- white puone icon
+6774880886 -- blue phone
+-- gun
+137342102999169 -- gun icon
+101264601033096 -- white ak 4
+
+--[[if u dont know how to use then give up
+  but
+  rbxassetid://id here (gotta be numbers)
+  ]]--
